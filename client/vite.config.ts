61e2a80bf1ba5f8c5c5c452911ab219a9b51b395
IFactory <ilkin.nezerov@gmail.com>
2023-03-13 18:44:23 +0400

    [IMP-6]: IMP-6: Create DockerFile and add Node, Nginx Containers, and set up docker-compose file
    - set up absolute import
    - set up Nginx for development mode
    - add optimized Dockerfile instruction for production
    - add nginx configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
+  },
+  server: {
+    port: 3000,
+    open: true,
+    cors: true,
+  }
 })
