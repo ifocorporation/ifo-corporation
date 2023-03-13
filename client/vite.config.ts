5375bfc485e834541bf6a61ab0ef15c5360af60e
IFactory <ilkin.nezerov@gmail.com>
2023-03-13 19:58:28 +0400

    IMP-6]: Create DockerFile and add Node, Nginx Containers, and set up… … docker-compose file
    - optimize vite-config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'esm',
+        manualChunks: () => 'everything.js'
+      }
+    }
+  },
   resolve: {
     alias: {
       src: "/src",
