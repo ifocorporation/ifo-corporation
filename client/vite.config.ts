f6e03106a68a57418cec11c1e0087184688f679b
IFactory <ilkin.nezerov@gmail.com>
2023-03-13 23:46:06 +0400

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
+  base:"/ifo-corporation/client",
   build: {
+    outDir: 'docs',
     rollupOptions: {
       output: {
         format: 'esm',
@@ -27,5 +29,4 @@ export default defineConfig({
   server: {
     port: 3000
   },
-  base:"/ifo-corporation"
 })
