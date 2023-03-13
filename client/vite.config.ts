2b72ead8834a7eb9c22491710e9dbaf74149ee4a
IFactory <ilkin.nezerov@gmail.com>
2023-03-13 23:50:58 +0400

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base:"/ifo-corporation/client",
+  base:"/client",
   build: {
     outDir: 'docs',
     rollupOptions: {
